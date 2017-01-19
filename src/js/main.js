f543036f6dfd7602cb82ea57638d7e6b39aae2e7
Timothy Luciani <onorinbejasus@gmail.com>
2017-01-19 00:16:16 -0600

    -- Initial Commit -- Added the PV Protein Viewer, Bootstrap, jQuery libraries -- Constructed the initial view with two side-by-side proteins

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+var App = App || {};
+
+(function(){
+
+  // create reference to 'this'
+  var self = this;
+
+  /* Starting point of the program. Initializes the application */
+  function init() {
+
+    // Default viewer options
+    var options = {
+      antialias: true,
+      quality : 'medium'
+    };
+
+    /* create the left and right viewers */
+    App.leftViewer = new MolecularViewer();
+    App.rightViewer = new MolecularViewer();
+
+    /* initialize the left/right viewers */
+    App.leftViewer.init( 'leftViewer', options );
+    App.rightViewer.init( 'rightViewer', options );
+
+    /* load the pdb file for each viewer */
+    App.leftViewer.loadFromRCMB('2YPI');
+    App.rightViewer.loadFromRCMB('5CKN');
+  }
+
+  /* start the application once the DOM is ready */
+  document.addEventListener('DOMContentLoaded', init);
+
+})();
---
+++ src/js/molecularViewer.js
@@ -0,0 +1,51 @@
+// global application variable
+var App = App || {};
+
+// Protein / Molecule Viewer "Class"
+var MolecularViewer = function(){
+
+  /* initialize the molecular viewer global variable */
+  var viewer = {};
+
+  function initialize(id, options) {
+
+    /* get the DOM element by the id parameter */
+    viewer.domObj = document.getElementById(id);
+
+    /* get/save the width and height of the given DOM element */
+    viewer.width = viewer.domObj.clientWidth;
+    viewer.height = viewer.width * 0.9;
+
+    /* add the width and height to the options */
+    options.width = viewer.width;
+    options.height = viewer.height;
+
+    /* insert the viewer under the DOM element */
+    viewer.pvViewer = pv.Viewer(document.getElementById(id), options);
+  }
+
+  function loadPDBFromRCMB(proteinName){
+
+    /* perform an async download from RCMB to fetch the requested PDB File */
+    pv.io.fetchPdb('https://files.rcsb.org/download/' + proteinName + '.pdb', function(structure) {
+
+      /* display the protein as cartoon, coloring the secondary structure
+         elements in a rainbow gradient */
+      viewer.pvViewer.cartoon(proteinName, structure, { color : color.ssSuccession() });
+
+      /* center the structure in the view */
+      // center in viewer
+      viewer.pvViewer.centerOn(structure);
+      // auto zoom to fit
+      viewer.pvViewer.autoZoom();
+
+    });
+  }
+
+  /* return the public-facing functions */
+  return {
+    init          : initialize,
+    loadFromRCMB  : loadPDBFromRCMB
+  };
+
+};

c8a1031ddb53e9019a20ba5c7c252a3dfc4103d3
Timothy Luciani <onorinbejasus@gmail.com>
2017-01-26 23:20:16 -0600

    -- Cleaned up the code a little

--- src/js/algorithms/SequenceSorting.js
+++ src/js/algorithms/SequenceSorting.js
@@ -9,7 +9,7 @@ var SequenceSorting = function(family){
   /* Internal private variable*/
   let self = {};
   /* Promise */
-  self.frequencies_Computed = null;
+  self.frequencies_computed = null;
 
   /* Calculates the frequency of each column's residues*/
   function calculate_fragment_frequency(){
@@ -21,14 +21,14 @@ var SequenceSorting = function(family){
     self.frequencies = [];
 
     /* Store the promise for later use */
-    self.frequencies_Computed = new Promise(function(resolve, reject){
+    self.frequencies_computed = new Promise(function(resolve, reject){
       /* Iterate over each column to determine the frequencies of residues*/
       for(let i = 0; i < max_len; i++){
         /* Get the residues of the ith column */
-        let col = _.map(family, function(o) { return _.nth(o.sequence, i); });
+        let column_residues = _.map(family, function(o) { return _.nth(o.sequence, i); });
 
         /* Get the residue counts for each column*/
-        self.frequencies[i] = _.countBy(col);
+        self.frequencies[i] = _.countBy(column_residues);
       }
       /* resolve the promise with the column frequencies*/
       resolve(self.frequencies)

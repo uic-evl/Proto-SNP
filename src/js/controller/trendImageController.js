5a6ee7a42aa31e80cb45e8e6f838e008a884aab0
onorinbejasus <tluciani21@gmail.com>
2017-01-27 15:08:45 -0600

    -- Refactored the trend image viewer and controller ---- Split up the brush creation and attachment into seperate functions
    -- Highlighted the first row of the trend image by default

--- src/js/controller/trendImageController.js
+++ src/js/controller/trendImageController.js
@@ -8,34 +8,39 @@ var TrendImageController = function(){
 
   let self = {};
 
-  function horizontalPaddleController(residue_size, yScale) {
+  function horizontalPaddleController(residue_glyph_size, yScale) {
 
-    if (!d3.event.sourceEvent) return; // Only transition after input.
-    if (d3.event.sourceEvent.type === "brush") return; // if the event isn't a brushing
+    /* We only want to capture user events. */
+    if (!d3.event.sourceEvent) {
+      /* The first event that fires is the initialization. */
+      self.previousHorizontalSelection = d3.event.selection.map(yScale.invert)[0];
+      return;
+    }
     if (!d3.event.selection) return; // Ignore empty selections.
+    if (d3.event.sourceEvent.type === "brush") return; // if the event isn't associated with a mousemove
 
     // Round the two event extents to the nearest row
-    d3.event.selection[0] = Math.ceil(d3.event.selection[0]/residue_size)*residue_size;
-    d3.event.selection[1] = Math.ceil(d3.event.selection[1]/residue_size)*residue_size;
+    d3.event.selection[0] = Math.ceil(d3.event.selection[0]/residue_glyph_size)*residue_glyph_size;
+    d3.event.selection[1] = Math.ceil(d3.event.selection[1]/residue_glyph_size)*residue_glyph_size;
 
     // Snap the brush onto the closest protein
     d3.select(this).call(d3.event.target.move, d3.event.selection);
 
     /* Get the current protein */
-    let protein = d3.event.selection.map(yScale.invert)[0];
+    let currentHorizontalSelection = d3.event.selection.map(yScale.invert)[0];
 
     /* Reset the opacity of the previous row */
-    if(self.previousHorizontal){
-      d3.selectAll('.p' + self.previousHorizontal)
-          .attr("class", "p" + self.previousHorizontal);
+    if(self.previousHorizontalSelection){
+      d3.selectAll('.p' + self.previousHorizontalSelection)
+          .attr("class", "p" + self.previousHorizontalSelection);
     }
 
     /* Set the previous horizontal to the current protein */
-    self.previousHorizontal = protein;
+    self.previousHorizontalSelection = currentHorizontalSelection;
 
     /* Set the opacity of the highlighted row */
-    d3.selectAll('.p' + protein)
-        .attr("class", "p" + protein + " active-selection");
+    d3.selectAll('.p' + currentHorizontalSelection)
+        .attr("class", "p" + currentHorizontalSelection + " active-selection");
 
   }
 
@@ -46,15 +51,15 @@ var TrendImageController = function(){
 
   }
 
-  function verticalPaddleController(residue_size) {
+  function verticalPaddleController(residue_glyph_size) {
 
     if (!d3.event.sourceEvent) return; // Only transition after input.
     if (d3.event.sourceEvent.type === "brush") return; // if the event isn't a brushing
     if (!d3.event.selection) return; // Ignore empty selections.
 
     // Round the two event extents to the nearest row
-    d3.event.selection[0] = Math.ceil(d3.event.selection[0]/residue_size)*residue_size;
-    d3.event.selection[1] = Math.ceil(d3.event.selection[1]/residue_size)*residue_size;
+    d3.event.selection[0] = Math.ceil(d3.event.selection[0]/residue_glyph_size)*residue_glyph_size;
+    d3.event.selection[1] = Math.ceil(d3.event.selection[1]/residue_glyph_size)*residue_glyph_size;
 
     // Snap the brush onto the closest protein
     d3.select(this).call(d3.event.target.move, d3.event.selection);
--- src/js/view/trendImageViewer.js
+++ src/js/view/trendImageViewer.js
@@ -67,55 +67,95 @@ var TrendImageViewer = function(){
       ;
   }
 
+  /* Function to create the three brush paddles*/
+  function createBrushes(x_axis_length, y_axis_length, xScale, yScale, protein_family_data) {
+
+    /* Get the halfway point of the x-range*/
+    let x_midpoint = xScale(x_axis_length/2);
+
+    /* Construct the horizontal Protein-selection paddle */
+    trendImageViewer.horizonalPaddle = d3.brushY()
+      .extent( [ [0, 0], [trendImageViewer.width, y_axis_length * trendImageViewer.residue_glyph_size] ])
+      // .on("start", trendImageViewer.controller.horizontalStart)
+      .on("brush", function(){ trendImageViewer.controller.horizontalBrushed.call(this, trendImageViewer.residue_glyph_size, yScale) })
+      .on("end", function(){trendImageViewer.controller.horizontalEnd.call(this, yScale)})
+    ;
+
+    // /* Construct the right vertical residue-selection paddle */
+    // trendImageViewer.leftVerticalPaddle = d3.brushX()
+    //   .extent( [ [0, 0], [x_midpoint, y_axis_length * trendImageViewer.residue_glyph_size] ])
+    //   .on("brush", function(){ trendImageViewer.controller.verticalBrushed.call(this, trendImageViewer.residue_glyph_size) })
+    //   .on("end", function(){trendImageViewer.controller.verticalEnd.call(this, xScale, protein_family_data)})
+    // ;
+    //
+    // /* Construct the right vertical residue-selection paddle */
+    // trendImageViewer.rightVerticalPaddle = d3.brushX()
+    //   .extent( [ [x_midpoint, 0], [trendImageViewer.width, y_axis_length * trendImageViewer.residue_glyph_size] ])
+    //   .on("brush", function(){ trendImageViewer.controller.verticalBrushed.call(this, trendImageViewer.residue_glyph_size) })
+    // ;
+  }
+
+  /* Function to add the three brush paddles to the svg*/
+  function addBrushes(y_axis_length) {
+    /* Multiple Brushes help: http://bl.ocks.org/jssolichin/54b4995bd68275691a23*/
+    trendImageViewer.brushes = trendImageViewer.svg.append("g")
+      .attr("class", "brushes")
+      .style("width", trendImageViewer.width)
+      .style("height", trendImageViewer.residue_glyph_size * y_axis_length);
+
+    /* Add the horizontal paddle to the trend image */
+    trendImageViewer.brushes.append("g")
+      .attr("class", "brush horizontal")
+      .call(trendImageViewer.horizonalPaddle) // add the paddle
+      .call(trendImageViewer.horizonalPaddle.move, [0, trendImageViewer.residue_glyph_size]) // initialize the position
+    ;
+
+    // /* Add the left vertical paddle to the trend image*/
+    // trendImageViewer.brushes.append("g")
+    //   .attr("class", "brush vertical-left")
+    //   .call(trendImageViewer.leftVerticalPaddle)
+    //   .call(trendImageViewer.leftVerticalPaddle.move, [0, trendImageViewer.residue_glyph_size])
+    // ;
+
+    /* Add the right vertical paddle to the trend image*/
+    // trendImageViewer.brushes.append("g")
+    //     .attr("class", "brush vertical-right")
+    //     .call(trendImageViewer.rightVerticalPaddle)
+    //     .call(trendImageViewer.rightVerticalPaddle.move, [0, trendImageViewer.residue_glyph_size])
+    //     // HACK: There is an issue with the rush extent that doesn't start at 0
+    //     .select("g.brush>.selection")
+    //     .attr("x", Math.ceil((trendImageViewer.width/2)/trendImageViewer.residue_glyph_size)*trendImageViewer.residue_glyph_size)
+    // ;
+  }
+
   function render(family) {
 
     /* Get the length of the longest sequence */
-    let max_sequence_length = _.max(d3.set(family.map(function( residue ) { return residue.length; } )).values());
+    let x_axis_length = _.max(d3.set(family.map(function( residue ) { return residue.length; } )).values());
 
     /* Extract the names of the proteins. They will construct the y-axis*/
     let y_elements = d3.set(family.map(function( residue ) { return residue.name; } )).values();
 
     /* Get and save the size of each residue for the trend image based on the width of the screen */
-    trendImageViewer.residue_size = Math.round(trendImageViewer.width / max_sequence_length);
+    trendImageViewer.residue_glyph_size = Math.round(trendImageViewer.width / x_axis_length);
 
     /* construct the y-scale */
     let yScale = d3.scaleBand()
             .domain(y_elements)
-            .range([0, y_elements.length * trendImageViewer.residue_size])
+            .range([0, y_elements.length * trendImageViewer.residue_glyph_size])
         ;
 
     /* construct the x-scale */
     let xScale = d3.scaleLinear()
-            .domain([0, max_sequence_length])
-            .range([0, Math.ceil((trendImageViewer.width-1)/trendImageViewer.residue_size)*trendImageViewer.residue_size])
+            .domain([0, x_axis_length])
+            .range([0, Math.ceil((trendImageViewer.width-1)/trendImageViewer.residue_glyph_size)*trendImageViewer.residue_glyph_size])
         ;
 
-    /* Get the halfway point of the x-range*/
-    let xMid = xScale(max_sequence_length/2);
+    /* Create the three brushes */
+    createBrushes(x_axis_length, y_elements.length, xScale, yScale);
 
     /* Construct a color map using the residue codes*/
     let residueModel = new ResidueModel();
-
-    /* Construct the horizontal Protein-selection paddle */
-    trendImageViewer.horizonalPaddle = d3.brushY()
-      .extent( [ [0, 0], [trendImageViewer.width, y_elements.length * trendImageViewer.residue_size] ])
-      // .on("start", trendImageViewer.controller.horizontalStart)
-      .on("brush", function(){ trendImageViewer.controller.horizontalBrushed.call(this, trendImageViewer.residue_size, yScale) })
-      .on("end", function(){trendImageViewer.controller.horizontalEnd.call(this, yScale)})
-      ;
-
-    /* Construct the right vertical residue-selection paddle */
-    trendImageViewer.leftVerticalPaddle = d3.brushX()
-        .extent( [ [0, 0], [xMid, y_elements.length * trendImageViewer.residue_size] ])
-        .on("brush", function(){ trendImageViewer.controller.verticalBrushed.call(this, trendImageViewer.residue_size) })
-        .on("end", function(){trendImageViewer.controller.verticalEnd.call(this, xScale, family)})
-        ;
-
-    /* Construct the right vertical residue-selection paddle */
-    trendImageViewer.rightVerticalPaddle = d3.brushX()
-        .extent( [ [xMid, 0], [trendImageViewer.width, y_elements.length * trendImageViewer.residue_size] ])
-        .on("brush", function(){ trendImageViewer.controller.verticalBrushed.call(this, trendImageViewer.residue_size) })
-    ;
 
     /* Invoke the tip in the context of your visualization */
     //trendImageViewer.svg.call(trendImageViewer.tooltip);
@@ -131,8 +171,8 @@ var TrendImageViewer = function(){
               .attr("class", "aminoAcid")
               .append('rect')
               .attr("class", function(d, i) { return "p" + d.protein + " r" + i; })
-              .attr("width", trendImageViewer.residue_size)
-              .attr("height", trendImageViewer.residue_size)
+              .attr("width", trendImageViewer.residue_glyph_size)
+              .attr("height", trendImageViewer.residue_glyph_size)
               .attr('y', function(d) { return yScale(d.protein) })
               .attr('x', function(d) { return xScale(d.x) })
               .attr('fill', function(d) { return residueModel.getColor(d.residue); })
@@ -141,35 +181,8 @@ var TrendImageViewer = function(){
             // .on('mouseout', trendImageViewer.tooltip.hide)
           ;
 
-          /* Multiple Brushes help: http://bl.ocks.org/jssolichin/54b4995bd68275691a23*/
-          trendImageViewer.brushes = trendImageViewer.svg.append("g")
-            .attr("class", "brushes")
-            .style("width", trendImageViewer.width)
-            .style("height", trendImageViewer.residue_size * y_elements.length);
-
-          /* Add the horizontal paddle to the trend image */
-          trendImageViewer.brushes.append("g")
-              .attr("class", "brush horizontal")
-              .call(trendImageViewer.horizonalPaddle) // add the paddle
-              .call(trendImageViewer.horizonalPaddle.move, [0, trendImageViewer.residue_size]) // initialize the position
-          ;
-
-          /* Add the left vertical paddle to the trend image*/
-          trendImageViewer.brushes.append("g")
-              .attr("class", "brush vertical-left")
-              .call(trendImageViewer.leftVerticalPaddle)
-              .call(trendImageViewer.leftVerticalPaddle.move, [0, trendImageViewer.residue_size])
-          ;
-
-          /* Add the right vertical paddle to the trend image*/
-          // trendImageViewer.brushes.append("g")
-          //     .attr("class", "brush vertical-right")
-          //     .call(trendImageViewer.rightVerticalPaddle)
-          //     .call(trendImageViewer.rightVerticalPaddle.move, [0, trendImageViewer.residue_size])
-          //     // HACK: There is an issue with the rush extent that doesn't start at 0
-          //     .select("g.brush>.selection")
-          //     .attr("x", Math.ceil((trendImageViewer.width/2)/trendImageViewer.residue_size)*trendImageViewer.residue_size)
-          // ;
+          /*Add the brushes to the trend image*/
+          addBrushes(y_elements.length);
 
           /* Remove the pointer events from the brush overlays to prevent:
            * 1: Deleting the brush on a wrong click
@@ -181,8 +194,9 @@ var TrendImageViewer = function(){
           trendImageViewer.brushes.selectAll('.selection')
               .style("shape-rendering", "auto");
 
-          // trendImageViewer.brushes.select("g.brush.vertical-right")
-          // .call(trendImageViewer.rightVerticalPaddle.move, [-trendImageViewer.width/2, trendImageViewer.residue_size]);
+          /* Set the first highlighted row's opacity */
+          d3.selectAll('.p' + y_elements[0])
+            .attr("class", "p" +  y_elements[0] + " active-selection");
 
         });
   }

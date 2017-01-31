4dcc01d28346ac3f85dc180000486947a127fe1f
onorinbejasus <tluciani21@gmail.com>
2017-01-31 16:19:53 -0600

    -- Refactored molecular viewer a little

--- src/js/view/molecularViewer.js
+++ src/js/view/molecularViewer.js
@@ -9,10 +9,10 @@ var MolecularViewer = function(){
   /* initialize the molecular viewer global variable */
   let molecularViewer = {};
 
-  function initialize(id, options) {
+  function initialize(div_id, options) {
 
     /* get the DOM element by the id parameter */
-    molecularViewer.domObj = d3.select(id).node();
+    molecularViewer.domObj = d3.select(div_id).node();
 
     /* check if viewing options were passed in */
     options = options || {};
@@ -26,13 +26,13 @@ var MolecularViewer = function(){
     options.height = molecularViewer.height;
 
     /* insert the molecularViewer under the DOM element */
-    molecularViewer.pvViewer = pv.Viewer(d3.select(id).node(), options);
+    molecularViewer.pvViewer = pv.Viewer(d3.select(div_id).node(), options);
   }
 
   function render(structure, proteinName) {
 
     /* Display the protein as cartoon, coloring the secondary structure
-       elements in a rainbow gradient */
+     elements in a rainbow gradient */
     molecularViewer.pvViewer.cartoon(proteinName, structure, { color : color.ssSuccession() });
 
     /* center the structure in the view */
@@ -42,33 +42,33 @@ var MolecularViewer = function(){
     molecularViewer.pvViewer.autoZoom();
   }
 
-/* Accessor to get the underlying structure in the molecularViewer */
-function get_structure() { return molecularViewer.structure; }
+  /* Accessor to get the underlying structure in the molecularViewer */
+  function get_structure() { return molecularViewer.structure; }
 
-/* Accessor to get the molecularViewer's height and width */
-function get_dimensions() { return {width: molecularViewer.width, height: molecularViewer.height }}
+  /* Accessor to get the molecularViewer's height and width */
+  function get_dimensions() { return {width: molecularViewer.width, height: molecularViewer.height }}
 
-/* Accessor to get the underlying sequence of the Protein*/
-function get_sequence(structure, chain) {
-  // Array to store the sequence
-  let seq = [];
+  /* Accessor to get the underlying sequence of the Protein*/
+  function get_sequence(structure, chain) {
+    // Array to store the sequence
+    let seq = [];
 
-  /* Iterate over the residues of the chain and add them to the array*/
-  structure.chains()[chain || 0].eachResidue(function(res){
-    seq.push(res.name());
-  });
+    /* Iterate over the residues of the chain and add them to the array*/
+    structure.chains()[chain || 0].eachResidue(function(res){
+      seq.push(res.name());
+    });
 
-  // return the sequence
-  return seq;
-}
+    // return the sequence
+    return seq;
+  }
 
-/* return the public-facing functions */
-return {
-  init          : initialize,
-  render        : render,
-  getStructure  : get_structure,
-  getSequence   : get_sequence,
-  getDimensions : get_dimensions
-};
+  /* return the public-facing functions */
+  return {
+    init          : initialize,
+    render        : render,
+    getStructure  : get_structure,
+    getSequence   : get_sequence,
+    getDimensions : get_dimensions
+  };
 
 };

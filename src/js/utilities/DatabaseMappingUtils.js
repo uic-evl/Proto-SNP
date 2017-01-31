1824d8e66bd18ace4afea2804d58a3685f1fb4c8
onorinbejasus <tluciani21@gmail.com>
2017-01-31 17:07:44 -0600

    -- Added a dictionary to store all of the mapped proteins already queried

--- src/js/utilities/DatabaseMappingUtils.js
+++ src/js/utilities/DatabaseMappingUtils.js
@@ -9,6 +9,9 @@ var DatabaseMappingUtils = function(){
   /* Global Class Variable*/
   var mappingUtils = {};
 
+  /* A dictionary for all the mapped proteins that were previously queried */
+  mappingUtils.mappedProteins = {};
+
   /* Map from uniprot 'mneumonic' to PDB code*/
   function map_mneumonic_to_PDB(mneumonic){
 

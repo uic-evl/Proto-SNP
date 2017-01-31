da50b25e62b43d18c8b9d9889d551489e520745d
onorinbejasus <tluciani21@gmail.com>
2017-01-31 16:57:58 -0600

    -- Added a new library to map between different protein database name conventions

---
+++ src/js/utilities/DatabaseMappingUtils.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// global application variable
+var App = App || {};
+
+// Utilities class for mapping protein names
+var DatabaseMappingUtils = function(){
+
+  /* Global Class Variable*/
+  var mappingUtils = {};
+
+  /* Map from uniprot 'mneumonic' to PDB code*/
+  function map_mneumonic_to_PDB(mneumonic){
+
+    // http://www.uniprot.org/uniprot/?query=mneumonic:SHRM_DROME+AND+database:pdb&format=tab&compress=no&columns=id,database(PDB)
+
+  }
+
+  return {
+    mneumonicToPDB : map_mneumonic_to_PDB
+  };
+
+};

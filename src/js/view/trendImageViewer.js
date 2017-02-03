325af5a91d47190297c7bed69a6283a75d814e51
Timothy Luciani <onorinbejasus@gmail.com>
2017-02-02 22:07:45 -0600

    -- Rearranged a few functions in the file

--- src/js/view/trendImageViewer.js
+++ src/js/view/trendImageViewer.js
@@ -35,55 +35,6 @@ const TrendImageViewer = function(){
 
   }
 
-  function initialize_chart_dom(){
-    /* get/save the width and height of the given DOM element */
-    set_chart_dimensions();
-
-    /* clear the trend image DOM */
-    clear_chart_dom();
-
-    /* Add the svg to the trend image dom*/
-    create_chart_svg();
-  }
-
-  /* Initialize the data descriptors -- protein names and sizes*/
-  function initialize_data_descriptors() {
-    /* Extract and save the protein names from the family*/
-    set_protein_names();
-
-    /* Set the dimensions of the trend image */
-    set_data_dimensions_sizes();
-  }
-
-  /* Initialize the trend image object, create globals, and create the svg */
-  function initialize(div_id) {
-
-    /* Set the DOM element by the id parameter */
-    set_chart_dom_obj(div_id);
-
-    /* Initialize the chart dOM*/
-    initialize_chart_dom();
-
-    /* initialize the proteins names and dimensions */
-    initialize_data_descriptors();
-
-    /* Get and save the size of each residue for the trend image based on the width of the screen */
-    set_glyph_size();
-
-    /* Initialize the scales for the trend image*/
-    set_chart_scales();
-
-    /* Trend Image interaction controller */
-    set_trend_image_controller();
-
-    /* Set the size of the initial vertical paddles */
-    trendImageViewer.verticalPaddleSize = 6;
-
-
-    /* Create the three brushes */
-    create_brushes();
-  }
-
   /* Clear the chart DOM of all elements */
   function clear_chart_dom() {
     trendImageViewer.domObj.selectAll().remove();
@@ -97,77 +48,6 @@ const TrendImageViewer = function(){
       .style("width", trendImageViewer.width)
       .style("height", trendImageViewer.height)
     ;
-  }
-
-  /* Render the trend image to the svg */
-  function render() {
-
-    /* Construct a color map using the residue codes*/
-    let residueModel = new ResidueModel();
-
-    /* Invoke the tip in the context of your visualization */
-    //trendImageViewer.svg.call(trendImageViewer.tooltip);
-
-    /* Convert the data into a residue-based array */
-    map_trend_image_data()
-      .then(function(data){
-        /* Construct the image out of each residue  */
-        trendImageViewer.svg.append("g")
-          .selectAll("rect")
-          .data(data)
-          .enter().append('g')
-          .attr("class", "aminoAcid")
-          .append('rect')
-          .attr("class", function(d, i) { return "p" + d.protein + " r" + i % trendImageViewer.x_axis_length; })
-          .attr("width", trendImageViewer.residue_glyph_size)
-          .attr("height", trendImageViewer.residue_glyph_size)
-          .attr('y', function(d) { return trendImageViewer.yScale(d.protein) })
-          .attr('x', function(d) { return trendImageViewer.xScale(d.x) })
-          .attr('fill', function(d) { return residueModel.getColor(d.residue); })
-          .attr('stroke',  function(d) { return residueModel.getColor(d.residue); })
-        // .on('mouseover', trendImageViewer.tooltip.show)
-        // .on('mouseout', trendImageViewer.tooltip.hide)
-        ;
-
-        /*Add the brushes to the trend image*/
-        add_brushes();
-
-        /* Remove the pointer events from the brush overlays to prevent:
-         * 1: Deleting the brush on a wrong click
-         * 2: Interference between brushes
-         */
-        trendImageViewer.brushes.selectAll('.overlay')
-          .style("pointer-events", "none");
-
-        /* Let d3 decide the best rendering for the brushes */
-        trendImageViewer.brushes.selectAll('.selection')
-          .style("shape-rendering", "auto");
-
-        /* Set the context menu of the vertical brush */
-        trendImageViewer.brushes.select("g.brush.horizontal")
-          .on("contextmenu", d3.contextMenu(create_context_menu));
-
-        /* Highlight the initial selections*/
-
-        /* Set the first highlighted row's opacity */
-        d3.selectAll('.p' + get_protein_names()[0])
-          .classed("active_protein_selection", true);
-
-        /* Iterate over the left selection and add the active class to the selected fragments */
-        for(let i = 0; i < trendImageViewer.verticalPaddleSize; i++) {
-          d3.selectAll("rect[class$='r" + parseInt(i) + "']")
-            .classed("vertical-left", true)
-            .classed("active_res_selection", true);
-        }
-
-        /* Iterate over the right selection and add the active class to the selected fragments */
-        for(let i = trendImageViewer.x_axis_length - trendImageViewer.verticalPaddleSize ; i < trendImageViewer.x_axis_length; i++) {
-          d3.selectAll("rect[class$='r" + parseInt(i) + "']")
-            .classed("vertical-right", true)
-            .classed("active_res_selection", true);
-        }
-
-      });
   }
 
   /* Create a customized context menu per right-click */
@@ -257,6 +137,41 @@ const TrendImageViewer = function(){
       initialize_frequency_viewers();
   }
 
+  /* Function to add the three brush paddles to the svg*/
+  function add_brushes() {
+
+    /* Multiple Brushes help: http://bl.ocks.org/jssolichin/54b4995bd68275691a23*/
+    trendImageViewer.brushes = trendImageViewer.svg.append("g")
+        .attr("class", "brushes")
+        .style("width", trendImageViewer.width)
+        .style("height", trendImageViewer.residue_glyph_size * trendImageViewer.y_axis_length);
+
+    /* Add the horizontal paddle to the trend image */
+    trendImageViewer.brushes.append("g")
+        .attr("class", "brush horizontal")
+        .call(trendImageViewer.horizonalPaddle.brush) // add the paddle
+        // initialize the position
+        .call(trendImageViewer.horizonalPaddle.brush.move, [0, trendImageViewer.residue_glyph_size])
+    ;
+
+    /* Add the left vertical paddle to the trend image*/
+    trendImageViewer.brushes.append("g")
+        .attr("class", "brush vertical-left")
+        .call(trendImageViewer.leftVerticalPaddle.brush)
+        // initialize the position
+        .call(trendImageViewer.leftVerticalPaddle.brush.move, [0, trendImageViewer.residue_glyph_size * trendImageViewer.verticalPaddleSize])
+    ;
+
+    /* Add the right vertical paddle to the trend image*/
+    trendImageViewer.brushes.append("g")
+        .attr("class", "brush vertical-right")
+        .call(trendImageViewer.rightVerticalPaddle.brush)
+        // initialize the position
+        .call(trendImageViewer.rightVerticalPaddle.brush.move,
+            [trendImageViewer.width - trendImageViewer.residue_glyph_size * trendImageViewer.verticalPaddleSize, trendImageViewer.width])
+    ;
+  }
+
   /* Function to create the three brush paddles*/
   function create_brushes() {
     /* Construct the horizontal Protein-selection paddle */
@@ -290,53 +205,135 @@ const TrendImageViewer = function(){
       .then(enable_vertical_brushing);
   }
 
-  /* Function to add the three brush paddles to the svg*/
-  function add_brushes() {
+  /* Render the trend image to the svg */
+  function render() {
 
-    /* Multiple Brushes help: http://bl.ocks.org/jssolichin/54b4995bd68275691a23*/
-    trendImageViewer.brushes = trendImageViewer.svg.append("g")
-      .attr("class", "brushes")
-      .style("width", trendImageViewer.width)
-      .style("height", trendImageViewer.residue_glyph_size * trendImageViewer.y_axis_length);
+    /* Construct a color map using the residue codes*/
+    let residueModel = new ResidueModel();
 
-    /* Add the horizontal paddle to the trend image */
-    trendImageViewer.brushes.append("g")
-      .attr("class", "brush horizontal")
-      .call(trendImageViewer.horizonalPaddle.brush) // add the paddle
-      // initialize the position
-      .call(trendImageViewer.horizonalPaddle.brush.move, [0, trendImageViewer.residue_glyph_size])
-    ;
+    /* Invoke the tip in the context of your visualization */
+    //trendImageViewer.svg.call(trendImageViewer.tooltip);
 
-    /* Add the left vertical paddle to the trend image*/
-    trendImageViewer.brushes.append("g")
-      .attr("class", "brush vertical-left")
-      .call(trendImageViewer.leftVerticalPaddle.brush)
-      // initialize the position
-      .call(trendImageViewer.leftVerticalPaddle.brush.move, [0, trendImageViewer.residue_glyph_size * trendImageViewer.verticalPaddleSize])
-    ;
+    /* Convert the data into a residue-based array */
+    map_trend_image_data()
+        .then(function(data){
+          /* Construct the image out of each residue  */
+          trendImageViewer.svg.append("g")
+              .selectAll("rect")
+              .data(data)
+              .enter().append('g')
+              .attr("class", "aminoAcid")
+              .append('rect')
+              .attr("class", function(d, i) { return "p" + d.protein + " r" + i % trendImageViewer.x_axis_length; })
+              .attr("width", trendImageViewer.residue_glyph_size)
+              .attr("height", trendImageViewer.residue_glyph_size)
+              .attr('y', function(d) { return trendImageViewer.yScale(d.protein) })
+              .attr('x', function(d) { return trendImageViewer.xScale(d.x) })
+              .attr('fill', function(d) { return residueModel.getColor(d.residue); })
+              .attr('stroke',  function(d) { return residueModel.getColor(d.residue); })
+          // .on('mouseover', trendImageViewer.tooltip.show)
+          // .on('mouseout', trendImageViewer.tooltip.hide)
+          ;
 
-    /* Add the right vertical paddle to the trend image*/
-    trendImageViewer.brushes.append("g")
-      .attr("class", "brush vertical-right")
-      .call(trendImageViewer.rightVerticalPaddle.brush)
-      // initialize the position
-      .call(trendImageViewer.rightVerticalPaddle.brush.move,
-        [trendImageViewer.width - trendImageViewer.residue_glyph_size * trendImageViewer.verticalPaddleSize, trendImageViewer.width])
-    ;
+          /*Add the brushes to the trend image*/
+          add_brushes();
+
+          /* Remove the pointer events from the brush overlays to prevent:
+           * 1: Deleting the brush on a wrong click
+           * 2: Interference between brushes
+           */
+          trendImageViewer.brushes.selectAll('.overlay')
+              .style("pointer-events", "none");
+
+          /* Let d3 decide the best rendering for the brushes */
+          trendImageViewer.brushes.selectAll('.selection')
+              .style("shape-rendering", "auto");
+
+          /* Set the context menu of the vertical brush */
+          trendImageViewer.brushes.select("g.brush.horizontal")
+              .on("contextmenu", d3.contextMenu(create_context_menu));
+
+          /* Highlight the initial selections*/
+
+          /* Set the first highlighted row's opacity */
+          d3.selectAll('.p' + get_protein_names()[0])
+              .classed("active_protein_selection", true);
+
+          /* Iterate over the left selection and add the active class to the selected fragments */
+          for(let i = 0; i < trendImageViewer.verticalPaddleSize; i++) {
+            d3.selectAll("rect[class$='r" + parseInt(i) + "']")
+                .classed("vertical-left", true)
+                .classed("active_res_selection", true);
+          }
+
+          /* Iterate over the right selection and add the active class to the selected fragments */
+          for(let i = trendImageViewer.x_axis_length - trendImageViewer.verticalPaddleSize ; i < trendImageViewer.x_axis_length; i++) {
+            d3.selectAll("rect[class$='r" + parseInt(i) + "']")
+                .classed("vertical-right", true)
+                .classed("active_res_selection", true);
+          }
+
+        });
+  }
+
+  function initialize_chart_dom(){
+    /* get/save the width and height of the given DOM element */
+    set_chart_dimensions();
+
+    /* clear the trend image DOM */
+    clear_chart_dom();
+
+    /* Add the svg to the trend image dom*/
+    create_chart_svg();
+  }
+
+  /* Initialize the data descriptors -- protein names and sizes*/
+  function initialize_data_descriptors() {
+    /* Extract and save the protein names from the family*/
+    set_protein_names();
+
+    /* Set the dimensions of the trend image */
+    set_data_dimensions_sizes();
+  }
+
+  /* Initialize the trend image object, create globals, and create the svg */
+  function initialize(div_id) {
+
+    /* Set the DOM element by the id parameter */
+    set_chart_dom_obj(div_id);
+
+    /* Initialize the chart dOM*/
+    initialize_chart_dom();
+
+    /* initialize the proteins names and dimensions */
+    initialize_data_descriptors();
+
+    /* Get and save the size of each residue for the trend image based on the width of the screen */
+    set_glyph_size();
+
+    /* Initialize the scales for the trend image*/
+    set_chart_scales();
+
+    /* Trend Image interaction controller */
+    set_trend_image_controller();
+
+    /* Set the size of the initial vertical paddles */
+    trendImageViewer.verticalPaddleSize = 6;
+
+
+    /* Create the three brushes */
+    create_brushes();
   }
 
   /*******************************************************************************************************************/
   /************                         Instance Accessors (Getters and Setters)                          ************/
   /*******************************************************************************************************************/
 
+  /************ Setters ************/
+
   /* Setter for the column frequency data */
   function set_column_frequency_data(column_frequencies) {
     trendImageViewer.column_frequencies = column_frequencies
-  }
-
-  /* Getter for the column frequency data */
-  function get_column_frequency_data() {
-    return trendImageViewer.column_frequencies;
   }
 
   /* Setter for the trend image controller */
@@ -362,12 +359,6 @@ const TrendImageViewer = function(){
     ;
   }
 
-  /* Getter for the x-Axis scale */
-  function get_x_axis_scale() { return trendImageViewer.xScale; }
-
-  /* Getter for the y-Axis scale */
-  function get_y_axis_scale() { return trendImageViewer.yScale; }
-
   /* Setter for the chart dimensions */
   function set_chart_dimensions() {
     trendImageViewer.width = App.trendWidth;
@@ -379,21 +370,47 @@ const TrendImageViewer = function(){
     trendImageViewer.protein_family_data = protein_family;
   }
 
-  /* Getter for the protein family data*/
-  function get_protein_family_data() {
-    return trendImageViewer.protein_family_data ;
-  }
-
   /* Set the dimensions of the data */
   function set_data_dimensions_sizes() {
 
     /* Get/store the length of the longest sequence */
     trendImageViewer.x_axis_length = _.max(d3.set(trendImageViewer.protein_family_data.map(
-      function( residue ) { return residue.length; } ))
-      .values());
+        function( residue ) { return residue.length; } ))
+        .values());
 
     /* Get/store the length of the y-axis -- i.e. how many proteins it contains */
     trendImageViewer.y_axis_length = get_protein_names().length;
+  }
+
+  /* Setter for the names of the proteins from the family */
+  function set_protein_names() {
+    trendImageViewer.proteinNames = d3.set(
+        trendImageViewer.protein_family_data.map(function( residue ) { return residue.name; } ))
+        .values();
+  }
+
+  /* Setter for the names of the proteins from the family */
+  function set_glyph_size() {
+    /* Get and save the size of each residue for the trend image based on the width of the screen */
+    trendImageViewer.residue_glyph_size = Math.round( trendImageViewer.width / trendImageViewer.x_axis_length);
+  }
+
+  /************ Getters ************/
+
+  /* Getter for the column frequency data */
+  function get_column_frequency_data() {
+    return trendImageViewer.column_frequencies;
+  }
+
+  /* Getter for the x-Axis scale */
+  function get_x_axis_scale() { return trendImageViewer.xScale; }
+
+  /* Getter for the y-Axis scale */
+  function get_y_axis_scale() { return trendImageViewer.yScale; }
+
+  /* Getter for the protein family data*/
+  function get_protein_family_data() {
+    return trendImageViewer.protein_family_data ;
   }
 
   /* Getter for the x-dimension size */
@@ -404,19 +421,6 @@ const TrendImageViewer = function(){
 
   /* Getter for the names of the proteins from the family */
   function get_protein_names() { return trendImageViewer.proteinNames; }
-
-  /* Setter for the names of the proteins from the family */
-  function set_protein_names() {
-    trendImageViewer.proteinNames = d3.set(
-      trendImageViewer.protein_family_data.map(function( residue ) { return residue.name; } ))
-      .values();
-  }
-
-  /* Setter for the names of the proteins from the family */
-  function set_glyph_size() {
-    /* Get and save the size of each residue for the trend image based on the width of the screen */
-    trendImageViewer.residue_glyph_size = Math.round( trendImageViewer.width / trendImageViewer.x_axis_length);
-  }
 
   /* Getter for the trend image glyph size */
   function get_glyph_size(){ return trendImageViewer.residue_glyph_size; }

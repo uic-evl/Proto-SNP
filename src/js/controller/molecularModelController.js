958efc1f3b42e5b9664e6d9da1724453aba19f13
Timothy Luciani <onorinbejasus@gmail.com>
2017-04-14 23:11:40 -0500

    -- Added a selection event to the molecular viewers -- Add a global application listener and registered a keyboard listener -- Added a keyboard event to zoom into selection (will most likely removed later for menu interactions)

--- src/js/controller/molecularModelController.js
+++ src/js/controller/molecularModelController.js
@@ -8,13 +8,23 @@ const MolecularModelController = function(options) {
 
   /* Class private variable */
   let self = {
-    prevPicked : null
+    prevPicked   : null
   };
 
   let viewer   = options.viewer;
   let parent   = options.parent;
-
   let label   = options.label;
+
+  function zoomToSelections () {
+
+    let allSelections = [];
+    viewer.forEach(function(go) {
+      if (go.selection !== undefined) {
+        allSelections.push(go.selection());
+      }
+    });
+    viewer.fitTo(allSelections);
+  }
 
   function setColorForAtom(go, atom, color) {
     let view = go.structure().createEmptyView();
@@ -27,15 +37,19 @@ const MolecularModelController = function(options) {
     let rect = viewer.boundingClientRect();
     let picked = viewer.pick({ x : event.clientX - rect.left,
       y : event.clientY - rect.top });
+
     if (self.prevPicked !== null && picked !== null &&
         picked.target() === self.prevPicked.atom) {
       return;
     }
+    /* If we're hovering over a molecule */
     if (self.prevPicked !== null) {
       // reset color of previously picked atom.
       setColorForAtom(self.prevPicked.node, self.prevPicked.atom, self.prevPicked.color);
     }
+
     if (picked !== null) {
+
       let atom = picked.target();
       label.innerHTML = atom.qualifiedName();
       // get RGBA color and store in the color array, so we know what it was
@@ -45,17 +59,49 @@ const MolecularModelController = function(options) {
       self.prevPicked = { atom : atom, color : color, node : picked.node() };
 
       setColorForAtom(picked.node(), atom, 'red');
-    } else {
+    }
+
+    else {
       label.innerHTML = '&nbsp;';
       self.prevPicked = null;
     }
     viewer.requestRedraw();
   }
 
+  function mouseClickEvent(picked, ev){
+    if (picked === null || picked.target() === null) {
+      return;
+    }
+    // don't to anything if the clicked structure does not have an atom.
+    if (picked.node().structure === undefined) {
+      return;
+    }
+    // when the shift key is pressed, extend the selection, otherwise
+    // only select the clicked atom.
+    let extendSelection = ev.shiftKey;
+    let sel;
+    if (extendSelection) {
+      sel = picked.node().selection();
+    } else {
+      sel = picked.node().structure().createEmptyView();
+    }
+    // in case atom was not part of the view, we have to add it, because
+    // it wasn't selected before. Otherwise removeAtom took care of it
+    // and we don't have to do anything.
+    if (!sel.removeAtom(picked.target(), true)) {
+      sel.addAtom(picked.target());
+    }
+    picked.node().setSelection(sel);
+    viewer.requestRedraw();
+  }
+
   /* Initialize the events attached to the 3D viewer */
   function initialize() {
-
     parent.addEventListener('mousemove', mouseMoveEvent);
+    viewer.on('click', mouseClickEvent);
+
+    /* Register the enter key to reset the selections of the view */
+    App.ApplicationCallbackListener.addKeyboardCallback(13, zoomToSelections);
 
   }
 

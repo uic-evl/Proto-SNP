3bf407ee896e01503f676268dd5c8d82b4dc750e
onorinbejasus <tluciani21@gmail.com>
2017-06-22 16:20:50 -0500

    -- Added the cube to the viewer

--- src/js/MVC/views/AxisView3D.js
+++ src/js/MVC/views/AxisView3D.js
@@ -11,128 +11,46 @@ const AxisView3D = (function() {
     /* Set a reference to the DOM element*/
     self.container = options.div;
 
-    self._setup_mouse = function() {
-      // $("canvas").mousemove(function (event) {
-      //   if (document.elementFromPoint) {
-      //     var $canvas = $(this);
-      //     // hide canvas visibility
-      //     // don't do display:none as we want to maintain canvas layout
-      //     $canvas.css('visibility', 'hidden');
-      //     // get the element underneath, if any
-      //     var $under = $(document.elementFromPoint(event.clientX, event.clientY));
-      //     // show again the canvas
-      //     $canvas.css('visibility', 'visible');
-      //     if ($under.hasClass('underneath')) {
-      //       $under.triggerHandler('mouseover');
-      //     } else {
-      //       $("#result").text("mouse is over the canvas");
-      //     }
-      //   }
-      // })
-    };
+    var camera, scene, renderer;
+    var mesh;
+    
+    init();
+    animate();
+    
+    function init() {
+      camera = new THREE.PerspectiveCamera( 70, options.width / options.height, 1, 1000 );
+      camera.position.z = 400;
+      scene = new THREE.Scene();
+      var texture = new THREE.TextureLoader().load( 'images/anterior.png' );
+      var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+      var material = new THREE.MeshBasicMaterial( { map: texture } );
+      mesh = new THREE.Mesh( geometry, material );
+      scene.add( mesh );
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio( window.devicePixelRatio );
+      renderer.setSize( options.width, options.height );
+      /* Attach the renderer to the DOM */
+      $(self.container).append(renderer.domElement);      //
 
-    self._create_axis_cube = function(){
-      // let maxAnisotropy = self.renderer.getMaxAnisotropy();
-      //
-      // let textureLoader = new THREE.TextureLoader();
-      //
-      // let texture0 = textureLoader.load('images/anterior.png'), // xpos, Right
-      //     texture1 = textureLoader.load('images/posterior.png'), // xneg, Left
-      //     texture2 = textureLoader.load('images/superior.png'), // ypos, Top
-      //     texture3 = textureLoader.load('images/inferior.png'), // yneg, Bottom
-      //     texture4 = textureLoader.load('images/right.png'), // zpos, Back
-      //     texture5 = textureLoader.load('images/left.png'); // zneg, Front
-      //
-      // texture0.anisotropy = maxAnisotropy;
-      // texture1.anisotropy = maxAnisotropy;
-      // texture2.anisotropy = maxAnisotropy;
-      // texture3.anisotropy = maxAnisotropy;
-      // texture4.anisotropy = maxAnisotropy;
-      // texture5.anisotropy = maxAnisotropy;
-      //
-      // let materialArray = [
-      //   new THREE.MeshBasicMaterial({
-      //     map: texture0
-      //   }),
-      //   new THREE.MeshBasicMaterial({
-      //     map: texture1
-      //   }),
-      //   new THREE.MeshBasicMaterial({
-      //     map: texture2
-      //   }),
-      //   new THREE.MeshBasicMaterial({
-      //     map: texture3
-      //   }),
-      //   new THREE.MeshBasicMaterial({
-      //     map: texture4
-      //   }),
-      //   new THREE.MeshBasicMaterial({
-      //     map: texture5
-      //   })
-      // ];
+    }
 
-      // orientation marker, patient coordinate system
-      // let
-      //     // MovingCubeMat = new THREE.MultiMaterial(materialArray),
-          // geometry = THREE.BoxBufferGeometry(15, 15, 15, 1, 1, 1);
-          // MovingCubeGeom = new THREE.Mesh(15, 15, 15, 1, 1, 1, materialArray);
+    function onWindowResize() {
+      camera.aspect = options.width / options.height;
+      camera.updateProjectionMatrix();
+      renderer.setSize( options.width, options.height );
+    }
 
-      let geometry = new THREE.BoxGeometry(15, 15, 15);
-      let material = new THREE.MeshBasicMaterial(
-          {
-            color: 0x00ff00,
-            opacity: 0.9, // CHANGED
-            transparent: true
-          } );
-
-      return new THREE.Mesh(geometry, material);
-    };
-
-    /* initialize the view */
-    self.initialize(options);
-    self.render();
+    function animate() {
+      requestAnimationFrame( animate );
+      mesh.rotation.x += 0.005;
+      mesh.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    }
 
   }
 
   AxisView3D.prototype = {
 
-    initialize: function(options) {
-      /*set some camera attributes */
-      let VIEW_ANGLE = 75,
-          ASPECT = options.width/options.height,
-          NEAR = 1,
-          FAR = 10000;
-
-      //create camera and a scene
-      this.camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR );
-      this.camera.position.z = 25;
-      this.scene = new THREE.Scene();
-
-      /*create and start the WebGL renderer*/
-      this.renderer = new THREE.WebGLRenderer( { alpha: true } );
-      this.renderer.setSize(options.width, options.height);
-      this.renderer.setClearColor( 0x000000, 0 );
-
-      /* Attach the renderer to the DOM */
-      $(this.container).append(this.renderer.domElement);
-
-      let cube = this._create_axis_cube(),
-          light = new THREE.AmbientLight(0xffffff, 1.0); // white light
-
-      // cube.position.set(65, -65, -100);
-
-      this.scene.add(cube);
-      this.scene.add(light);
-      this.scene.add(this.camera);
-    },
-    
-    render: function() {
-      this.renderer.render( this.scene, this.camera );
-    },
-
-    redraw: function() {
-
-    }
 
   };
 
